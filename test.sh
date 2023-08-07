09a5be33d7e2638239bbe8095ea624e56ed43c25
James Luther <81767471+james-luther@users.noreply.github.com>
2023-08-07 12:18:55 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
+csvcut -c 1-8 Fluid-Attacks-Results.csv > 1.csv
+csvcut -c 9 Fluid-Attacks-Results.csv > snippet.csv
+csvcut -c 10 Fluid-Attacks-Results.csv > method.csv
 for i in $(csvcut -c 4 Fluid-Attacks-Results.csv | cut -d ":" -f2 | cut -d "/" -f1); do
   if awk "BEGIN {exit !($i < 4.0)}"; then
     echo "$now,Low" >> 3.csv;
@@ -9,3 +12,5 @@ for i in $(csvcut -c 4 Fluid-Attacks-Results.csv | cut -d ":" -f2 | cut -d "/" -
     echo "$now,Critical" >> 3.csv;
   fi
 done
+csvjoin 1.csv snippet.csv method.csv 3.csv > output.csv
+sed -i "1s/.*/Title,Cwe,Description,Cvss,Finding,Stream,Kind,Where,Snippet,Method,Date,Severity/" output.csv
